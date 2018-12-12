8f0b0a49463269164ad4e96ba6274c0b610e7d62
Murilo Antonio De Souza <1140974@sga.pucminas.br>
2018-12-12 21:35:25 -0200

    test03

---
+++ AtividadesAula02/Atividade2Parte1Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtividadeAula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Narijão!");
+        }
+    }
+}
